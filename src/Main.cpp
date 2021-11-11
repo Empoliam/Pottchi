fa420d5f73a87abd7664289c075b5a983c10cf18
Empoliam <liamjwright@btinternet.com>
2021-11-11 13:06:01 +0000

    CMake integration for windows

--- src/Main.cpp
+++ src/Main.cpp
@@ -174,7 +174,7 @@ int simInit(int argc, char* argv[]) {
 			("maxMCS,i", po::value<unsigned int>()->default_value(1600), "Number of MCS")
 			("pixel,p", po::value<unsigned int>()->default_value(6), "Pixels per cell")
 			("height,h", po::value<unsigned int>()->default_value(125), "Simulation space height")
-			("width,w", po::value<unsigned int>()->default_value(50), "Simulation space width")
+			("width,w", po::value<unsigned int>()->default_value(125), "Simulation space width")
 			("delay,d", po::value<unsigned int>()->default_value(0), "Simulation artificial delay, arbitrary, play around for good values, zero for as fast as possible")
 			("fps,f", po::value<unsigned int>()->default_value(24), "Simulation target fps, default 24");
 
